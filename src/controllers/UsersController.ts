e358af56f83ad4a9093c2c848110e6e72e5906c8
Pedro <www.psro@gmail.com>
2020-09-15 14:44:08 -0300

    Rules when create users

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -1,2 +1,48 @@
 import { Request, Response } from 'express';
 import db from '../database/connection';
+
+export default class UsersController {
+    async index(request: Request, response: Response): Promise<Response> {
+        const users = await db('users');
+        return response.json(users);
+    }
+
+    async create(request: Request, response: Response): Promise<Response> {
+        const { name, username, bio, password, key } = request.body;
+
+
+        const trx = await db.transaction();
+
+		try {
+            const keyData = await trx('keys').where('password', key);
+            let { keyId, type, quantity } = keyData[0];
+
+			await trx('users').insert({
+                name, 
+                username, 
+                bio, 
+                password, 
+                type
+            });
+            
+            quantity--;
+            if (quantity == 0) {
+                await trx('keys').delete(keyId);
+            } else {
+                await trx('keys').update({
+                    password: key,
+                    type,
+                    quantity                
+                });
+            }
+			await trx.commit();
+
+			return response.status(201).send();
+		} catch (error) {
+			await trx.rollback();
+			return response.status(400).json({
+				error: 'Erro ao criar usuário',
+			});
+		}
+    }
+}
