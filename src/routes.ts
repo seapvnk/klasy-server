9e3e74b16ea017acc4579384dcffec1d682525c8
Pedro <www.psro@gmail.com>
2020-09-15 14:44:26 -0300

    routes updated

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,18 @@
 import express from 'express';
 import UsersController from './controllers/UsersController';
+import KeysController from './controllers/KeysController';
 
 const usersController = new UsersController();
+const keysController = new KeysController();
 
 const routes = express.Router();
 
+// Users
 routes.get('/users', usersController.index);
+routes.post('/users', usersController.create);
+
+// Users
+routes.get('/keys', keysController.index);
+routes.post('/keys', keysController.create);
 
 export default routes;
