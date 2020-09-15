5c1fce9a2d2df00c4f88fb9084ebc7c484e6372d
Pedro <www.psro@gmail.com>
2020-09-15 10:01:37 -0300

    Moving routes to api

--- routes/web.php
+++ routes/web.php
@@ -12,17 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::group(['prefix' => 'api'], function()
-{
-  Route::get('/', function (){
-    return response()->json(['message' => 'Klasy API',
-                             'status' => 'Connected']);
-  });
-
-});
-
-
-Route::get('/', function () {
-    return redirect('api');
-});
